5633c69d9b2cd37793a3ee35dbaf7f84890c9800
pac0455 <franhidalc@gmail.com>
2024-04-08 13:48:11 +0200

    estilos

--- src/Components/LeftBar/Layout.jsx
+++ src/Components/LeftBar/Layout.jsx
@@ -1,8 +1,15 @@
+import "./Layout.css"
+import { FaHome } from "react-icons/fa";
+
 const Layout = () => {
     return (
         <>
-        <h1>Hello World</h1>
+            <div className="navbar">
+                <div className="navbar_icon">
+                    <FaHome className="navbar_icon_item"/>
+                </div>
+            </div>
         </>
-     );
+    );
 }
 export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,7 @@
 import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet
-} from "react-router-dom";
-import { Layout } from './Components/index.js'
-const AppLayout = () => {
-  return (
-    <>
-      <Layout/>
-      <Outlet/>
-    </>
-  )
-}
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout/>,
-    children:[
-      {
-        path:'example',
-        element:(<h1>Example</h1>)
-      }
-    ]
-  },
-]);
-
+import './index.css'
+import {  RouterProvider} from "react-router-dom";
+import router from './router/index.jsx'
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
